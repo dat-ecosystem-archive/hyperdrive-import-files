c431e62f6732897371bb65739ad97b0cf246240d
Julian Gruber <julian@juliangruber.com>
2016-07-11 17:37:53 +0200

    add example

---
+++ example.js
@@ -0,0 +1,11 @@
+const hyperdrive = require('hyperdrive')
+const memdb = require('memdb')
+const hyperImport = require('.')
+
+const drive = hyperdrive(memdb())
+const archive = drive.createArchive()
+
+hyperImport(archive, process.argv.slice(2), { live: true }, err => {
+  console.log('done')
+  archive.list({ live: true }).on('data', console.log)
+})
