e2765b42c4102eedc1d566a2f7413776f8ea7bf0
Julian Gruber <julian@juliangruber.com>
2017-02-16 11:45:22 +0100

    fix example

--- example.js
+++ example.js
@@ -5,7 +5,9 @@ var hyperImport = require('.')
 var drive = hyperdrive(memdb())
 var archive = drive.createArchive()
 
-var status = hyperImport(archive, process.argv.slice(2), { live: true }, function (err) {
+var target = process.argv.slice(2)[0]
+
+var status = hyperImport(archive, target, { live: true }, function (err) {
   if (err) throw err
   console.log('done')
   console.log('file count', status.fileCount)
